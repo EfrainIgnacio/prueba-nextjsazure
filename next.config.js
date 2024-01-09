a6ff17fa9bda83b9154ae68ff59cdc493c94e498
EfrainIgnacio <126102694+EfrainIgnacio@users.noreply.github.com>
2024-01-09 12:36:08 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  //   exportTrailingSlash: true,
+  //   exportPathMap: function () {
+  //     return {
+  //       "/": { page: "/" },
+  //     }
+  //   },
+}
 
 module.exports = nextConfig
